d69dd8f2f1b44bec46f1dd6ce48e592fa978f327
Ehsan <ehsansafavi1384@gmail.com>
2025-04-08 14:08:12 +0330

    Update and rename Q7.java to Q07.java

--- Q07.java
+++ Q07.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Q7 {
+public class Q07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
